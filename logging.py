dee44b84c5f1c2b4ea2d72a16ef6ba4016fd9f82
elfurrita <eduardofurrazola@gmail.com>
2025-04-08 03:59:37 +0200

    Update logging.py

--- logging.py
+++ logging.py
@@ -1,1 +1,9 @@
+# logging.py
 
+import logging
+
+def setup_logging():
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        handlers=[logging.FileHandler("trading_bot.log"),
+                                  logging.StreamHandler()])
